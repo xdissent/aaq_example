191a75fba7d49f113db289575e8fc4c7ac1668ac
Greg Thornton <xdissent@me.com>
2013-01-16 12:29:41 -0600

    Update aaq and simplify everything.:

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -3,15 +3,9 @@ require_dependency 'widget'
 class WidgetsController < ApplicationController
   layout "base"
 
-  include QueryableHelper
-  helper :queryable
-
-  query_class WidgetQuery
-
-  before_filter :find_query_object
+  queryable
 
   def index
-    @widgets = @query.query
     render :layout => !request.xhr?
   end
 end
